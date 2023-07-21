fc4fc5c63f3d967f61a31830e96384522ac8c593
Junhao <tjunhao17@gmail.com>
2023-07-21 15:50:03 +0800

    .

--- script.js
+++ script.js
@@ -1,52 +1,84 @@
-//target all elements to save to constants
+
+
+// Target all elements to save to constants
 const page1btn = document.querySelector("#page1btn");
 const page2btn = document.querySelector("#page2btn");
 const page3btn = document.querySelector("#page3btn");
 var allpages = document.querySelectorAll(".page");
-//select all subtopic pages
+// Select all subtopic pages
 console.log(allpages);
+
+// Target the DARTS heading
+const dartsHeading = document.querySelector("h1");
 
 hideall();
 
-function hideall() { //function to hide all pages
-    for (let onepage of allpages) { //go through all subtopic pages
-        onepage.style.display = "none"; //hide it
+function hideall() {
+    // Function to hide all pages
+    for (let onepage of allpages) {
+        onepage.style.display = "none";
     }
+
+    // Toggle the visibility of the DARTS heading
+    toggleDartsHeading();
 }
+
 function show(pgno) {
-    // Get the selected page based on the parameter passed in
     let onepage = document.querySelector("#page" + pgno);
-  
+
     // Check if the page is already visible
     if (onepage.style.display === "block") {
-      // If the page is visible, hide it
-      onepage.style.display = "none";
+        // If the page is visible, hide it with a fade-out effect
+        onepage.style.opacity = "0"; // Set opacity to 0 to start the fade-out effect
+        setTimeout(() => {
+            onepage.style.display = "none"; // Hide the element after the fade-out effect finishes
+            // Toggle the visibility of the DARTS heading when hiding the subtopic page
+            toggleDartsHeading();
+        }, 500); // Adjust the time (milliseconds) based on the CSS transition duration
     } else {
-      // If the page is hidden, hide all other pages and show this page
-      hideall(); // Hide all other pages
-      onepage.style.display = "block"; // Show the selected page
+        // If the page is hidden, hide all other pages and show this page with a fade-in effect
+        hideall(); // Hide all other pages
+        onepage.style.display = "block"; // Show the selected page
+        onepage.style.opacity = "0"; // Set opacity to 0 to start the fade-in effect
+
+        // Use setTimeout to give the browser some time to apply the "display: block" before starting the fade-in effect
+        setTimeout(() => {
+            onepage.style.opacity = "1"; // Set opacity to 1 to gradually fade-in the content
+        }, 50); // Adjust the time (milliseconds) based on the CSS transition delay
+
+        // Toggle the visibility of the DARTS heading when showing the subtopic page
+        toggleDartsHeading();
     }
-  }
+}
+
+function toggleDartsHeading() {
+    // Function to check the visibility of subtopic pages and toggle the DARTS heading
+    const isAnyPageVisible = Array.from(allpages).some((onepage) => {
+        return onepage.style.display === "block";
+    });
+
+    dartsHeading.style.display = isAnyPageVisible ? "none" : "block";
+}
+
 /*Listen for clicks on the buttons, assign anonymous
-eventhandler functions to call show function*/
+event handler functions to call the show function*/
 page1btn.addEventListener("click", function () {
     show(1);
-  });
-  page2btn.addEventListener("click", function () {
+});
+page2btn.addEventListener("click", function () {
     show(2);
-  });
-  page3btn.addEventListener("click", function () {
+});
+page3btn.addEventListener("click", function () {
     show(3);
-  });
+});
 
-
-/*for hamMenu */
+// For hamMenu
 const hamBtn = document.querySelector("#hamIcon");
 hamBtn.addEventListener("click", toggleMenus);
 const menuItemsList = document.querySelector("nav ul");
-function toggleMenus() { /*open and close menu*/
+
+function toggleMenus() {
+    // Function to open and close the menu
     menuItemsList.classList.toggle("menuShow");
-    // if (menuItemsList.style.display == "block")
-    //     menuItemsList.style.display = "none";
-    // else menuItemsList.style.display = "block";
-}//can optimize using toggle class with css transitions
+}
+
